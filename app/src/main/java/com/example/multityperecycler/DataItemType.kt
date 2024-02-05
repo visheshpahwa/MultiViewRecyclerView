9ff3e07b7ac6630ca114b52c4c1c3d778972e4d0
Vishesh Pahwa <vishesh.pahwa@grayopus.com>
2024-02-05 12:15:20 +0530

    multi recycler view

---
+++ app/src/main/java/com/example/multityperecycler/DataItemType.kt
@@ -0,0 +1,9 @@
+package com.example.multityperecycler
+
+class DataItemType {
+    companion object{
+        const val BEST_SELLER=0
+        const val CLOTHING=1
+        const val BANNER=2
+    }
+}
